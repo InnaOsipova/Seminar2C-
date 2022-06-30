e6442be58560f3c7c74c86e550fa45ae9ad4db04
Inna Osipova <rutheva_inna@mail.ru>
2022-06-30 21:20:52 +0300

    Третье число

---
+++ Ex002/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+
+void Showthird (string num)
+{
+  char [] array = num.ToCharArray();
+  int count = array.Length;
+ 
+  if (count > 2)
+  {
+     Console.WriteLine (array[2]);
+  }
+  else
+  {
+      Console.WriteLine("третьего числа нет");
+  }
+}
+
+Console.Write("Введите целое число : ");
+string number = Console.ReadLine();
+
+Showthird(number);
+
