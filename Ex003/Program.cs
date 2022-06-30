f88f7af31cb018c5dbbc6b8c7d7343707b26100a
Inna Osipova <rutheva_inna@mail.ru>
2022-06-30 22:07:39 +0300

    Ден недели

---
+++ Ex003/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день 
+//недели, и проверяет, является ли этот день выходным.
+
+void WeekEnd (int num)
+{
+   if(num>0 & num <8)
+   {  if(num < 6 )
+      {
+          Console.WriteLine(num+" - Этот день недели не выходной");
+      }
+      else
+      {
+          Console.WriteLine(num+ "- выходной - можно отдыхать");
+      }
+   }
+   else
+   {
+     Console.WriteLine("Это не день недели");
+   }
+}
+  
+
+Console.Write ("введите цифру дня недели : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+WeekEnd(number);
